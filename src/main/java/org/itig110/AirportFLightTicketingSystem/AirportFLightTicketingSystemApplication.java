ef36e82764c6272ffaf8bf9396b04e576f779018
MhmdShaker <mohamedshaker77565@gmail.com@gmail.com>
2022-10-01 22:08:48 +0200

    First Inializing

---
+++ src/main/java/org/itig110/AirportFLightTicketingSystem/AirportFLightTicketingSystemApplication.java
@@ -0,0 +1,13 @@
+package org.itig110.AirportFLightTicketingSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirportFLightTicketingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirportFLightTicketingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/itig110/AirportFLightTicketingSystem/AirportFLightTicketingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.itig110.AirportFLightTicketingSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirportFLightTicketingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
