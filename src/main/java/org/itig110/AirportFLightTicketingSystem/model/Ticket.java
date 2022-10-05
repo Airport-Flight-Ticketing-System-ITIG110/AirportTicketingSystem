18cf13e9dd99dfb0d474d82ac9a3c3bdab9a0a0e
SalmaAhmed2001 <salma2001ahmednady@gmail.com>
2022-10-05 23:27:45 +0200

    Adding ticket model

---
+++ src/main/java/org/itig110/AirportFLightTicketingSystem/model/Ticket.java
@@ -0,0 +1,22 @@
+package org.itig110.AirportFLightTicketingSystem.model;
+public class Ticket  {
+
+private String number;
+ private Passenger passenger;
+
+ public String getNumber() {
+  return number;
+ }
+
+ public void setNumber(String number) {
+  this.number = number;
+ }
+
+ public Passenger getPassenger() {
+  return passenger;
+ }
+
+ public void setPassenger(Passenger passenger) {
+  this.passenger = passenger;
+ }
+}
